52c70fc70937f2f0ccddd3304fe8ed65730e7ae3
Oriol <eltiempopaso@gmail.com>
2024-08-12 01:37:02 +0200

    initial commit

---
+++ build/zephyr/zephyr.dts.d
@@ -0,0 +1,17 @@
+empty_file.o: /home/elotro/zephyrproject/zephyr/misc/empty_file.c \
+ /home/elotro/zephyrproject/zephyr/boards/arduino/uno_r4/arduino_uno_r4_wifi.dts \
+ /home/elotro/zephyrproject/zephyr/boards/arduino/uno_r4/arduino_uno_r4_common.dtsi \
+ /home/elotro/zephyrproject/zephyr/dts/arm/renesas/ra/r7fa4m1ab3cfm.dtsi \
+ /home/elotro/zephyrproject/zephyr/include/zephyr/dt-bindings/clock/r7fa4m1xxxxxx-clock.h \
+ /home/elotro/zephyrproject/zephyr/include/zephyr/dt-bindings/clock/renesas-ra-cgc.h \
+ /home/elotro/zephyrproject/zephyr/dts/arm/renesas/ra/ra4-cm4-common.dtsi \
+ /home/elotro/zephyrproject/zephyr/dts/arm/renesas/ra/ra-cm4-common.dtsi \
+ /home/elotro/zephyrproject/zephyr/dts/common/mem.h \
+ /home/elotro/zephyrproject/zephyr/dts/common/freq.h \
+ /home/elotro/zephyrproject/zephyr/dts/arm/armv7-m.dtsi \
+ /home/elotro/zephyrproject/zephyr/dts/common/skeleton.dtsi \
+ /home/elotro/zephyrproject/zephyr/include/zephyr/dt-bindings/interrupt-controller/renesas-ra-icu.h \
+ /home/elotro/zephyrproject/zephyr/include/zephyr/dt-bindings/gpio/gpio.h \
+ /home/elotro/zephyrproject/zephyr/boards/arduino/uno_r4/arduino_uno_r4_wifi-pinctrl.dtsi \
+ /home/elotro/zephyrproject/zephyr/include/zephyr/dt-bindings/pinctrl/renesas/pinctrl-r7fa4m1xxxxxx.h \
+ /home/elotro/zephyrproject/zephyr/include/zephyr/dt-bindings/pinctrl/renesas/pinctrl-ra-common.h
