016cb9f3b366ef22203c15fbfca00234828b827f
Oriol <eltiempopaso@gmail.com>
2025-10-18 13:10:18 +0200

    moving workflow manager in independent file

---
+++ chessApp_python/lib/workflow_manager.py
@@ -0,0 +1,16 @@
+from kivy.uix.screenmanager import SlideTransition
+
+class WorkflowManager:
+    def __init__(self, screen_manager):
+        self.sm = screen_manager
+        self.state = "menu"
+
+    def go_to(self, state):
+        self.state = state
+        if state == "menu":
+            self.sm.transition = SlideTransition(direction='right')
+            self.sm.current = "menu"
+        elif state == "game":
+            self.sm.transition = SlideTransition(direction='left')
+            self.sm.current = "game"
+        # Add more states/screens as needed
--- chessApp_python/ui/ChessApp.py
+++ chessApp_python/ui/ChessApp.py
@@ -2,21 +2,7 @@ from kivy.app import App
 from ui.MenuScreen import MenuScreen
 from ui.ChessGameScreen import ChessGameScreen
 from kivy.uix.screenmanager import ScreenManager, SlideTransition
-
-class WorkflowManager:
-    def __init__(self, screen_manager):
-        self.sm = screen_manager
-        self.state = "menu"
-
-    def go_to(self, state):
-        self.state = state
-        if state == "menu":
-            self.sm.transition = SlideTransition(direction='right')
-            self.sm.current = "menu"
-        elif state == "game":
-            self.sm.transition = SlideTransition(direction='left')
-            self.sm.current = "game"
-        # Add more states/screens as needed
+from lib.workflow_manager import WorkflowManager
 
 class ChessApp(App):
     def build(self):
