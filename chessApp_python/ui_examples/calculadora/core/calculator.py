c2a9150a9c9bc15296f7b61a02339e4ae8379ae4
Oriol Parera Fiestas <eltiempopaso@gmail.com>
2026-01-20 15:04:38 +0100

    removing chessapp python as it has its own repo now

--- chessApp_python/ui_examples/calculadora/core/calculator.py
+++
@@ -1,9 +0,0 @@
-class Calculator:
-    """Pure logic: knows how to compute results, but no workflow knowledge."""
-
-    def add(self, a, b):
-        return a + b
-
-    def multiply(self, a, b):
-        return a * b
-
